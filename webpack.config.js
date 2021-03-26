9aa7b4b313dde8674d72067235c20ef00e6a45bb
macnick <nick@macexperts.gr>
2021-03-26 09:10:18 +0200

    Configure webpack for image and css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'app.js',
-    path: __dirname + 'dist',
+    path: path.resolve(__dirname + 'dist'),
     publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
   },
 };
