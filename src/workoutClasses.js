acf3c10e89c65754a2185f6e1c3a983d81fadf3b
macnick <nick@macexperts.gr>
2021-04-06 10:14:11 +0300

    Move classes to a new file

---
+++ src/workoutClasses.js
@@ -0,0 +1,47 @@
+class Workout {
+  date = new Date();
+  id = Date.now().toString().slice(-9);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+}
+
+export class Running extends Workout {
+  type = 'running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+    this._setDescription();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this;
+  }
+}
+
+export class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, distance, duration, elevGain) {
+    super(coords, distance, duration);
+    this.elevGain = elevGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+
+  calcSpeed() {
+    this.speed = (this.distance / this.duration) * 60;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  // entry: './src/index.js',
-  // output: {
-  //   filename: 'app.js',
-  //   path: path.resolve(__dirname + 'dist'),
-  //   publicPath: '/',
-  // },
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname + 'dist'),
+    publicPath: '/',
+  },
   module: {
     rules: [
       {
